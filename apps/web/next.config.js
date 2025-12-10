d6dd4e3651ce5a0152863bfca9ce06d30efc8dc4
Antonio Lopez <114641719+aalopez23@users.noreply.github.com>
2025-12-10 11:54:27 -0800

    Fix config syntax for output and reactStrictMode

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/pomodoro-app",
-  output = "export",
-  ReactStrictMode: true,
+  output: "export",
+  reactStrictMode: true,
   //transpilePackages: ['@pomodoro/utils'],
 };
 module.exports = nextConfig;
